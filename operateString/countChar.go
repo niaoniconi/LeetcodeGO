f5572e202b55265ad8392c133b7b8afbbba586f8
niconiconi <2897101433@qq.com>
2023-01-29 15:59:12 +0800

    update countAsterisk

--- operateString/countChar.go
+++ operateString/countChar.go
@@ -1,12 +1,15 @@
 package operateString
 
 func CountAsterisks(s string) int {
-	verticalBars:=0     //这个可以用flag来标识，因为只有两个状态
+	//verticalBars:=0     //这个可以用flag来标识，因为只有两个状态
+	flag:=true				//换了flag之后时间还变慢了，好怪
 	answer:=0
 	for _,value:=range s{
 		if value=='|'{
-			verticalBars=(verticalBars+1)%2
-		}else if verticalBars==0&&value=='*'{
+			flag=!flag
+			//verticalBars=(verticalBars+1)%2
+		//}else if verticalBars==0&&value=='*'{
+		}else if flag&&value=='*'{
 			answer++
 		}
 
