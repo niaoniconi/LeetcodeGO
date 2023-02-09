f469412a93ff9ec8a424124743659bf03b11aa21
niconiconi <2897101433@qq.com>
2023-02-09 18:03:07 +0800

    今天的一道题

--- operateString/patternMatch/prefix.go
+++ operateString/patternMatch/prefix.go
@@ -23,6 +23,7 @@ func RemoveSubfolders(folder []string)[]string{
 	return answer
 }
 
+// MatchFolderPrefix only for "/abc" folder
 func MatchFolderPrefix(folder string,pattern string) bool{
 	//没有重复的，等于可以排除
 	if len(folder)<=len(pattern){
