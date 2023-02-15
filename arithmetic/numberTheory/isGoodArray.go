b351cce67b225f849e099b2b45033e5bb6272748
niconiconi <2897101433@qq.com>
2023-02-15 21:03:17 +0800

    加上官方解

--- arithmetic/numberTheory/isGoodArray.go
+++ arithmetic/numberTheory/isGoodArray.go
@@ -6,7 +6,7 @@ import (
 
 // IsGoodArray  是否好数列
 /**
-其实我已经想到是找唯一共同的非1约数了，但是没想到直接用哦那个最大公约数算法，还以为最大公约数会很难
+其实我已经想到是找唯一共同的非1约数了，但是没想到直接用哦那个最大公约数算法，还以为最大公约数会很复杂
  */
 func IsGoodArray(nums []int)bool{
 	sort.Ints(nums)
@@ -51,4 +51,30 @@ func FindDivisors(num int) []int{
 		}
 	}
 	return divisors
+}
+
+// IsGoodArrayOfficial  官方解：使用gcd
+/**
+本来我想的是先用前两个数算出一个g，再用g和后面的数一起算，但是这样会很麻烦，需要考虑到的边界条件会很高
+直接设定0和任何数公约数是任何数会好很多，虽然也不知道公约数是不是这样规定的，行吧，0没有公约数
+其实可以g:=nums[0]，两个相同的数最大公约数是对方，
+g==1直接返回true这个也很棒，只要有一个不行就直接退出
+ */
+func IsGoodArrayOfficial(nums []int)bool{
+	g:=0
+	for _,value:=range nums{
+		g=GCD(g,value)
+		if g==1{
+			return true
+		}
+	}
+	return false
+}
+
+// GCD  0和x的公约数是x，辗转相除法
+func GCD(a,b int)int{
+	for a!=0{
+		a,b=b%a,a
+	}
+	return b
 }
