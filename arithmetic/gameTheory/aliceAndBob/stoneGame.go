ae286277933fe69e0ac6cad6c4550f69cf7195cf
sara <2897101433@qq.com>
2023-02-23 00:24:51 +0800

    记录一下链接

--- arithmetic/gameTheory/aliceAndBob/stoneGame.go
+++ arithmetic/gameTheory/aliceAndBob/stoneGame.go
@@ -1,5 +1,10 @@
 package aliceAndBob
-func StoneGameII(piles []int)int{
+
+// StoneGameII  https://leetcode.cn/problems/stone-game-ii/
+/**
+用动态规划可以写
+*/
+func StoneGameII(piles []int) int {
 
 	return 0
 }
