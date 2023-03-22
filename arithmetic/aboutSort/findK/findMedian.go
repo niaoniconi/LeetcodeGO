32fdf32354f16e0117989ef2076f09ae722e7e11
niconiconi <2897101433@qq.com>
2023-03-22 20:08:35 +0800

    一些测试和写题

--- arithmetic/aboutSort/findK/findMedian.go
+++ arithmetic/aboutSort/findK/findMedian.go
@@ -2,23 +2,60 @@ package findK
 
 // FindMedianSortedArrays https://leetcode.cn/problems/median-of-two-sorted-arrays/
 func FindMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+	if len(nums1)<len(nums2){
+		temp:=nums1
+		nums1=nums2
+		nums2=temp
+	}
 	m := len(nums1)
 	n := len(nums2)
-	if m == 1 && n == 1 {
-		return float64((nums2[0] + nums1[0]) / 2)
+	if n==0{
+		if m%2==0{
+			return float64(nums1[m/2-1]+nums1[m/2])/2
+		}else{
+			return float64(nums1[m/2])
+		}
 	}
-	if nums1[m-1] <= nums2[0] {
-		nums1 = append(nums1, nums2...)
-		return float64(nums1[(m+n-1)/2]+nums1[(m+n)/2]) / 2
+	for n>1{
+		if nums1[m/2]>nums2[n/2] {
+			nums1=nums1[:(m-n/2)]
+			nums2=nums2[n/2:]
+			m-=n/2
+			n-=n/2
+		} else {
+			nums1=nums1[n/2:]
+			nums2=nums2[:n-2/n]
+			m-=n/2
+			n-=n/2
+		}
 	}
-	if nums2[n-1] <= nums1[0] {
-		nums2 = append(nums2, nums1...)
-		return float64(nums2[(m+n-1)/2]+nums2[(m+n)/2]) / 2
+	if n==m{
+		return float64(nums1[0]+nums2[0])/2
 	}
+	if m%2==0{
+		if nums2[0]<nums1[m/2]{
+			if nums2[0]<nums1[m/2-1]{
+				return float64(nums1[m/2-1])
+			}else{
+				return float64(nums2[0])
+			}
+		}else{
+			return float64(nums1[m/2])
+		}
+	}else{
+		if nums2[0]<nums1[m/2]{
+			if nums2[0]<nums1[m/2-1]{
+				return float64(nums1[m/2-1]+nums1[m/2])/2
+			}else{
+				return float64(nums2[0]+nums1[m/2])/2
+			}
+		}else{
+			if nums2[0]<nums1[m/2+1]{
+				return float64(nums2[0]+nums1[m/2])/2
+			}else{
+				return float64(nums1[m/2+1]+nums1[m/2])/2
+			}
+		}
 
-	if nums1[m/2] <= nums2[n/2] {
-		return FindMedianSortedArrays(nums1[m/2:], nums2[:n/2])
-	} else {
-		return FindMedianSortedArrays(nums1[:m/2], nums2[n/2:])
 	}
 }
--- main.go
+++ main.go
@@ -91,4 +91,21 @@ func main() {
 	fmt.Println(customErrorPtr.Implements(typeOfError)) // #=> true
 	fmt.Println(customError.Implements(typeOfError)) // #=> false
 
+	ch1:=make(chan int,3)
+
+	//mutex:=&sync.Mutex{}
+	f:=func(i int){
+		//mutex.Lock()
+		fmt.Printf("the %v th goroutine", i)
+		ch1<-i
+	}
+	for i:=0;i<3;i++{
+		fmt.Println(i)
+		go f(i)
+	}
+
+	for i:=range ch1{
+		fmt.Printf("the %v th goroutine", i)
+	}
+
 }
